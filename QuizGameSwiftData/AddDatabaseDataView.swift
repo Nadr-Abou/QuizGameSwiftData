d70da33c9464cbeb4f481b7544057f59988ef810
Nadr-Abou <n.aboumoustafa@itsrizzoli.it>
2023-10-27 19:23:40 +0200

    Sucessive modifiche al db

---
+++ QuizGameSwiftData/AddDatabaseDataView.swift
@@ -0,0 +1,18 @@
+//
+//  addDatabaseDataView.swift
+//  QuizGameSwiftData
+//
+//  Created by user243551 on 10/27/23.
+//
+
+import SwiftUI
+
+struct addDatabaseDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addDatabaseDataView()
+}
